05679fa860210cf80a078fe42d3f9458c6e3de04
kruczynskim <milosz.kruczynski@ttsw.com.pl>
2025-09-18 15:50:40 +0200

    update investment_calculator

--- Go - The Complete Guide/Investment_calculator/investment_calculator.go
+++ Go - The Complete Guide/Investment_calculator/investment_calculator.go
@@ -6,10 +6,21 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	const inflationRate = 6.5
+	var investmentAmount, years, expectedReturnRate float64
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
