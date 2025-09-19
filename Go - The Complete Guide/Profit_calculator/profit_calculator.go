4544fdcd28f6b9b415803cee4d1a1c0ab1d4df64
kruczynskim <milosz.kruczynski@ttsw.com.pl>
2025-09-19 16:34:29 +0200

    modify profit_calculator.go - added error handling and write results to file

--- Go - The Complete Guide/Profit_calculator/profit_calculator.go
+++ Go - The Complete Guide/Profit_calculator/profit_calculator.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
+
+const writeFile = "calculations.txt"
 
 func main() {
 	var revenue, expenses, taxRate float64
 
-	getUserInput("Revenue: ", &revenue)
-	getUserInput("Expenses: ", &expenses)
-	getUserInput("Tax Rate: ", &taxRate)
+	errRevenue := getUserInput("Revenue: ", &revenue)
+	if errRevenue != nil {
+		panic(errRevenue)
+	}
+
+	errExpenses := getUserInput("Expenses: ", &expenses)
+	if errExpenses != nil {
+		panic(errExpenses)
+	}
+
+	errTaxRate := getUserInput("Tax Rate: ", &taxRate)
+	if errTaxRate != nil {
+		panic(errTaxRate)
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
+	storeResults(ebt, profit, ratio)
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
@@ -25,7 +41,22 @@ func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio
 	return
 }
 
-func getUserInput(text string, variable *float64) {
+func getUserInput(text string, variable *float64) error {
 	fmt.Print(text)
 	fmt.Scan(variable)
+
+	if *variable < 0.0 {
+		return errors.New(text + "You've entered negative number. ")
+	}
+
+	if *variable == 0.0 {
+		return errors.New(text + "You've entered 0.")
+	}
+
+	return nil
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	text := fmt.Sprintf("Ebt: %.1f Profit: %.1f Ratio: %.3f", ebt, profit, ratio)
+	os.WriteFile(writeFile, []byte(text), 0644)
 }
