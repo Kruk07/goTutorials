c056a27c1b88a9829b3adb1b4bf1214c2df0e9a0
kruczynskim <milosz.kruczynski@ttsw.com.pl>
2025-09-24 15:45:20 +0200

    update structs_practice - added interfaces

--- Go - The Complete Guide/Structs_practice/main.go
+++ Go - The Complete Guide/Structs_practice/main.go
@@ -7,10 +7,33 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
+type Saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	Saver
+	Display()
+}
+
 func main() {
+
+	printSomething(1)
+	printSomething(1.5)
+	printSomething("Hello!")
+
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text : ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +42,63 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the note failed.")
 		return
 	}
 
+	err = outputData(userNote)
+
+}
+
+func printSomething(value interface{}) {
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// }
+	intVal, ok := value.(int)
+
+	if !ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	float64Val, ok := value.(float64)
+
+	if !ok {
+		fmt.Println("Float:", float64Val)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if !ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	saveData(data)
+}
+
+func saveData(data Saver) error {
+
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return err
+	}
+
 	fmt.Println("Saving the note succeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
