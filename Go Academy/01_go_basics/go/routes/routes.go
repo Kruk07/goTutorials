fe926576607177f69c03b4b716509dedfa932460
kruczynskim <milosz.kruczynski@ttsw.com.pl>
2025-10-17 13:28:10 +0200

    feat: updated openapi.yaml to get certificates, added handlers

--- Go Academy/01_go_basics/go/routes/routes.go
+++ Go Academy/01_go_basics/go/routes/routes.go
@@ -3,8 +3,8 @@ package routes
 import (
 	"log"
 
-	"example.com/go_basics/go/api"      // wygenerowany kod z openapi.yaml
-	"example.com/go_basics/go/handlers" // Twoje handlery
+	"example.com/go_basics/go/api"
+	"example.com/go_basics/go/handlers"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,13 +12,11 @@ import (
 func NewEchoRouter(h *handlers.Handlers) *echo.Echo {
 	e := echo.New()
 
-	// Załaduj specyfikację OpenAPI
 	_, err := api.GetSwagger()
 	if err != nil {
 		log.Fatalf("Failed to load OpenAPI spec: %v", err)
 	}
 
-	// Rejestracja handlerów zgodnych z interfejsem ServerInterface
 	api.RegisterHandlers(e, h)
 
 	return e
